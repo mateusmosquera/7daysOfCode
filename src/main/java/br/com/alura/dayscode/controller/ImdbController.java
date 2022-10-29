e0f205771b0f0ba06a4f4b867a82e5e3522054d2
mateusmosquera <MateusMosquera@gitlab.com>
2022-10-29 15:19:28 -0300

    day 5 - ImdbService

--- src/main/java/br/com/alura/dayscode/controller/ImdbController.java
+++ src/main/java/br/com/alura/dayscode/controller/ImdbController.java
@@ -1,10 +1,9 @@
 package br.com.alura.dayscode.controller;
 
-import br.com.alura.dayscode.clients.ImdbClient;
 import br.com.alura.dayscode.domain.HTMLGenerator;
 import br.com.alura.dayscode.domain.ListOfMovies;
+import br.com.alura.dayscode.service.ImdbService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,24 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/imdb")
 public class ImdbController {
 
-    @Value("${imdb.api.key}")
-    private String apiKey;
-
     @Autowired
-    private ImdbClient imdbClient;
+    private ImdbService imdbService;
 
     @GetMapping("/top250")
     public ResponseEntity<ListOfMovies> getTop250() throws FileNotFoundException {
 
-        ResponseEntity<ListOfMovies> response = imdbClient.getTop250(apiKey);
+        ResponseEntity<ListOfMovies> response = imdbService.getTop250();
 
         PrintWriter writer = new PrintWriter("src/main/resources/content.html");
-        new HTMLGenerator(writer).generate(response.getBody());
+        new HTMLGenerator(writer).generate(Objects.requireNonNull(response.getBody()));
         writer.close();
 
         return response;
---
+++ src/main/java/br/com/alura/dayscode/service/ImdbService.java
@@ -0,0 +1,26 @@
+package br.com.alura.dayscode.service;
+
+import br.com.alura.dayscode.clients.ImdbClient;
+import br.com.alura.dayscode.domain.ListOfMovies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Service
+public class ImdbService {
+
+    @Value("${imdb.api.key}")
+    private String apiKey;
+
+    @Autowired
+    private ImdbClient imdbClient;
+
+    @GetMapping("/top250")
+    public ResponseEntity<ListOfMovies> getTop250()  {
+
+        return imdbClient.getTop250(apiKey);
+    }
+
+}
